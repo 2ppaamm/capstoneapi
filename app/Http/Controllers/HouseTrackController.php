aaa228e428ba6e2b005e8e531d8c67ab019df1ff
Pamela Lim <pamelaliusm@gmail.com>
2019-02-07 00:52:49 +0800

    for production

--- app/Http/Controllers/HouseTrackController.php
+++ app/Http/Controllers/HouseTrackController.php
@@ -41,12 +41,12 @@ class HouseTrackController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(CreateTrackRequest $request, House $houses)
+    public function store(CreateTrackRequest $request, House $house)
     {    
         $track = $request->all();
         $new_track = Auth::user()->tracks()->create($track);
-        $houses->tracks()->attach($new_track->id,['track_order'=>$houses->maxTrack($houses->id)? $houses->maxTrack($houses->id)->track_order + 1:1]);
-        return response()->json(['message' => 'Track correctly added', 'tracks'=>$houses->tracks,'code'=>201]);
+        $house->tracks()->attach($new_track->id,['track_order'=>$house->maxTrack($house->id)? $house->maxTrack($house->id)->track_order + 1:1]);
+        return response()->json(['message' => 'Track correctly added', 'tracks'=>$house->tracks,'code'=>201]);
     }
 
     /**
@@ -55,23 +55,11 @@ class HouseTrackController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(House $houses, Track $tracks)
+    public function show(House $house, Track $track)
     {
-        return $houses->tracks()->with('skills.questions')->whereTrackId($tracks->id)->get();   
+        return $house->tracks()->with('skills.questions')->whereTrackId($track->id)->get();   
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateRequest $request, House $houses, Track $tracks)
-    {
-        $msg =  $houses->tracks()->updateExistingPivot($tracks->id, [$request->get('field')=>$request->get('value')]) ? $request->field." successfully Updated" : "The track does not belong to the class.";
-        return response()->json(['message'=>$msg, 'house'=>$houses->tracks, 'code'=>201], 201);
-    }
 
     /**
      * Remove the tracks from the class.
