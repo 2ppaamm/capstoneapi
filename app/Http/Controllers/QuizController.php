b6b5e75e507747e54eb7cea3f4ee4f00efb65566
Pamela Lim <pamelaliusm@gmail.com>
2020-09-20 00:41:10 +0800

    quiz api done

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -29,14 +29,7 @@ class QuizController extends Controller
             return response()->json(['message' => 'Only administrators can access this api', 'code' => 403], 403);
         }
 
-        $quizzess = [];
-
-        foreach (Quiz::orderBy('id', 'desc')->get() as $quiz) {
-            $quizzess[] = $this->getQuiz($quiz->id);
-            continue;
-        }
-
-        return $quizzess;
+        return Quiz::with(['houses','questions'])->get();
     }
 
     /**
@@ -61,65 +54,24 @@ class QuizController extends Controller
         // add houses
         if ($request->exists('houses')) {
             $houses = array_wrap($request->get('houses'));
-            foreach ($houses as $house) {
-                HouseQuiz::create(array_merge($house, ['quiz_id' => $quiz->id]));
-            }
+            $quiz->houses()->sync($houses, false);
         }
 
         // add skills
         if ($request->exists('skills')) {
             $skills = array_wrap($request->get('skills'));
+            $quiz->skills()->sync($skills,false);
             foreach ($skills as $skill) {
-                QuizSkill::create(array_merge($skill, ['quiz_id' => $quiz->id]));
                 // add questions
-                $skillId = $skill['skill_id'];
-                $questionId = Question::where('skill_id', $skillId)->value('id');
+                $questionId = Question::where('skill_id', $skill)->whereSource('diagnostic')->value('id');
                 if ($questionId) {
-                    QuestionQuiz::create(['quiz_id' => $quiz->id, 'question_id' => $questionId]);
+                    $quiz->questions()->sync($questionId, false);
                 }
 
             }
         }
 
-        $id = $quiz->id;
-        return $this->getQuiz($id);
-    }
-
-    protected function getQuiz($id)
-    {
-        $quiz = Quiz::find($id);
-        $data = array_merge($quiz->toArray(), [
-            'houses' => [],
-            'questions' => [],
-            'skills' => [],
-        ]);
-
-        $houseIds = $quiz->houses->pluck('house_id');
-
-        if ($houseIds) {
-            $houses = House::whereIn('id', $houseIds)->get();
-            if ($houses) {
-                $data['houses'] = $houses;
-            }
-        }
-
-        $questionIds = $quiz->questions->pluck('question_id');
-        if ($questionIds) {
-            $questions = Question::whereIn('id', $questionIds)->get();
-            if ($questions) {
-                $data['questions'] = $questions;
-            }
-        }
-
-        $skillIds = $quiz->skills->pluck('skill_id');
-        if ($skillIds) {
-            $skills = Skill::whereIn('id', $skillIds)->get();
-            if ($skills) {
-                $data['skills'] = $skills;
-            }
-        }
-
-        return $data;
+        return $quiz->with('questions','houses')->get();
     }
 
     /**
@@ -149,31 +101,21 @@ class QuizController extends Controller
         }
 
         if ($request->exists('add_skills')) {
+            $quiz->skills()->sync($request->add_skills, false);
             foreach ($request->get('add_skills') as $skill) {
-                $skillId = $skill['skill_id'];
-
-                if (!Skill::where('id', $skillId)->count()) {
-                    continue;
-                }
-
-                if (QuizSkill::where(['quiz_id' => $quiz->id, 'skill_id' => $skillId])->count()) {
-                    continue;
-                }
-
-                // store quiz_skill table
-                QuizSkill::create(['quiz_id' => $quiz->id, 'skill_id' => $skillId]);
 
                 // store question in question_quiz table
-                $questionId = Question::where('skill_id', $skillId)->value('id');
+                $questionId = Question::where('skill_id', $skill)->value('id');
                 if (!$questionId) {
                     continue;
                 }
-                QuestionQuiz::create(['quiz_id' => $quiz->id, 'question_id' => $questionId]);
+                $quiz->questions()->sync([$questionId], false);
             }
         }
 
         if ($request->exists('remove_skills')) {
             foreach ($request->get('remove_skills') as $skill) {
+
                 $skillId = $skill['skill_id'];
                 QuizSkill::where(['quiz_id' => $quiz->id, 'skill_id' => $skillId])->delete();
 
@@ -228,7 +170,9 @@ class QuizController extends Controller
             }
         }
 
-        return $this->getQuiz($quiz->id);
+        $quiz->fill($request->except(['houses', 'skills']))->save();
+
+        return $quiz;
     }
 
     /**
@@ -243,9 +187,9 @@ class QuizController extends Controller
             return response()->json(['message' => 'There are classes or questions linked to this quiz, do you want to delink them first before deleting the quiz?', 'code' => 409], 409);
         }
 
-        $quiz->questions()->delete();
-        $quiz->skills()->delete();
-        $quiz->houses()->delete();
+        $quiz->questions()->detach();
+        $quiz->skills()->detach();
+        $quiz->houses()->detach();
         $quiz->delete();
 
         return response()->json(['message' => 'Quiz has been successfully deleted', 'code' => 201], 201);
@@ -256,31 +200,21 @@ class QuizController extends Controller
      */
     public function copy(Request $request, Quiz $quiz)
     {
-        $questions = $quiz->questions;
-        $skills = $quiz->skills;
-        $houses = $quiz->houses;
+        $questions = $quiz->questions()->pluck('id')->toArray();
+        $skills = $quiz->skills()->pluck('id')->toArray();
+        $houses = $quiz->houses()->pluck('id')->toArray();
 
-        unset($quiz['id'], $quiz['questions'], $quiz['skills'], $quiz['houses']);
+//        unset($quiz['id'], $quiz['questions'], $quiz['skills'], $quiz['houses']);
 
-        $quiz['quiz'] = 'copy' . $quiz['quiz'];
-        $newQuiz = Quiz::create($quiz->toArray());
+//        $quiz['quiz'] = 'copy' . $quiz['quiz'];
 
-        foreach ($questions as $question) {
-            $question['quiz_id'] = $newQuiz->id;
-            QuestionQuiz::create($question->toArray());
-        }
+        $newQuiz = Quiz::create(array_diff($quiz->toArray(), $quiz->pluck('id')->toArray()));
 
-        foreach ($skills as $skill) {
-            $skill['quiz_id'] = $newQuiz->id;
-            QuizSkill::create($skill->toArray());
-        }
+        $newQuiz->questions()->sync($questions, false);
+        $newQuiz->skills()->sync($skills, false);
+        $newQuiz->houses()->sync($houses, false);
 
-        foreach ($houses as $house) {
-            $house['quiz_id'] = $newQuiz->id;
-            HouseQuiz::create($house->toArray());
-        }
-
-        return $this->getQuiz($newQuiz->id);
+        return $newQuiz;
     }
 
     public function create()
