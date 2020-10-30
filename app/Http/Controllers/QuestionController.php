85f680a951fef857157f04d16625e81ae1a2f0db
Pamela Lim <pamelaliusm@gmail.com>
2020-10-30 13:00:56 +0800

    fix question_image bug

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -95,7 +95,7 @@ class QuestionController extends Controller
             $question['answer3'] = $request->answer3 == 'null'? NULL: (int)$request->answer3;
         }
 
-        if ($request->hasFile('question_image')) {
+        if ($request->question_image) {
             $q_image='q'.time().'.png';
             $file = $request->question_image->move(public_path().$image_path, $q_image);
             $question['question_image'] = '/images/questions/question_image/'.$q_image;
