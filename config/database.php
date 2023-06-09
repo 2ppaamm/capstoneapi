e274fae8199a263336297ef1c60985c8d9d066a0
Pamela <user@DESKTOP-PNL3EFM.(none)>
2023-06-09 08:59:07 +0800

    update from desktop

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'api'),
-            'username'  => env('DB_USERNAME', 'root'),
+            'username'  => env('DB_USERNAME', ''),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
